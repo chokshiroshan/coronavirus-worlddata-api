44d56bac0e49f81e8d2849b76c65e17757b2f03a
Roshan Chokshi <chokshiroshan@gmail.com>
2020-03-22 23:22:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,32 +12,35 @@ cors = CORS(app, resources={
        "origins": "*"
     }
 })
+def scrap():
+    link = "https://www.worldometers.info/coronavirus/"
 
-link = "https://www.worldometers.info/coronavirus/"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+    }
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-}
+    data = rq.get(link, headers=headers)
 
-data = rq.get(link, headers=headers)
+    soup = bs4.BeautifulSoup(data.content, "html.parser")
 
-soup = bs4.BeautifulSoup(data.content, "html.parser")
+    table = soup.find("table")
 
-table = soup.find("table")
+    d = {}
+    for row in table.find_all("tr"):
+        r = []
+        for data in row.find_all("td"):
+            r.append(data.text.strip())
+        # print(r)
+        try:
+            d[r[0]] = {'total': r[1], 'deaths': r[3], 'cured': r[5], 'active': r[6]}
+        except:
+            continue
 
-d = {}
-for row in table.find_all("tr"):
-    r = []
-    for data in row.find_all("td"):
-        r.append(data.text.strip())
-    # print(r)
-    try:
-        d[r[0]] = {'total': r[1], 'deaths': r[3], 'cured': r[5], 'active': r[6]}
-    except:
-        continue
+    return d
 
 class UserAPI(Resource):
     def get(self):
+        d = scrap()
         return jsonify(d)
 
 api.add_resource(UserAPI, '/')
